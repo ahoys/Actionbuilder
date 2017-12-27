8f2db849673f4158615c918738671074beaef802
Ari Höysniemi <ari.hoysniemi@gmail.com>
2017-12-27 21:53:42 +0200

    Improved repeater design.

--- @actionbuilder/addons/actionbuilder/module_REPEATER.hpp
+++ @actionbuilder/addons/actionbuilder/module_REPEATER.hpp
@@ -12,57 +12,136 @@ class RHNET_ab_moduleREPEATER_F: Module_F {
 	isDisposable = 0;
 
 	class Attributes: AttributesBase {
-		class ActivationType: Combo {
-			property = "RHNET_ab_moduleREPEATER_F_ActivationType";
+		class ActivationMethod: Combo {
+			property = "RHNET_ab_moduleREPEATER_F_ActivationMethod";
 			displayName = "Activated by";
 			tooltip = "How is the repeating triggered.";
 			typeName = "STRING";
 			defaultValue = """VARIABLE""";
 			class Values {
-				class RP_AT_BOOLEAN {
-					name = "Boolean Variable";
-					value = "BOOLEAN"
+				class RP_AT_VARIABLE {
+					name = "Boolean variable";
+					value = "VARIABLE"
 					default = 1;
 				};
-				class RP_AT_MIN_UNITCOUNT {
-					name = "Total unit count less than Value";
-					value = "MINUNITCOUNT";
-				};
-				class RP_AT_MAX_PLAYERCOUNT {
-					name = "Total player count more than Value";
-					value = "MAXPLAYERCOUNT";
+                class RP_AT_VALUE {
+					name = "Value";
+					value = "VALUE"
 				};
 			};
 		};
 
-        class CustomVariable: Edit {
-			property = "RHNET_ab_moduleREPEATER_F_CustomVariable";
-			displayName = "Variable";
-			tooltip = "Variable to be monitored.";
+        class BooleanDescription: Edit {
+			property = "RHNET_ab_moduleREPEATER_F_BooleanDescription";
+            description = "Boolean variable activator activates the repeater every time the given boolean switches to TRUE. If the variable is toggled, the variable will switch back to FALSE after a successful repeat.";
+			displayName = "";
+			tooltip = "";
+			control = "SubCategoryNoHeader2";
+            data = "AttributeSystemSubcategory"
+		};
+
+        class BooleanVariable: Edit {
+			property = "RHNET_ab_moduleREPEATER_F_BooleanVariable";
+			displayName = "Boolean variable";
+			tooltip = "Boolean variable to be monitored.";
 			typeName = "STRING";
 			control = "EditShort";
 		};
 
-        class CustomValue: Edit {
-			property = "RHNET_ab_moduleREPEATER_F_CustomValue";
+        class ToggleVariable: Checkbox {
+			property = "RHNET_ab_moduleREPEATER_F_ToggleVariable";
+			displayName = "Toggle variable";
+			tooltip = "Whether to switch Varible to FALSE after execution.";
+			typeName = "BOOL";
+            defaultValue = "true";
+		};
+
+        class ValueDescription: Edit {
+			property = "RHNET_ab_moduleREPEATER_F_ValueDescription";
+            description = "Value activator activates the repeater every time the given value condition is true.";
+			displayName = "";
+			tooltip = "";
+			control = "SubCategoryNoHeader2";
+            data = "AttributeSystemSubcategory"
+		};
+
+        class ValueCondition: Combo {
+			property = "RHNET_ab_moduleREPEATER_F_ValueCondition";
+			displayName = "Value condition";
+			tooltip = "What is the value limiter for the repeater to activate.";
+			typeName = "STRING";
+			defaultValue = """VARIABLE""";
+			class Values {
+                class RP_VT_MOREPLAYERS {
+					name = "More players alive than Value";
+					value = "PLAYERS"
+				};
+				class RP_VT_LESSUNITS {
+					name = "Fewer units alive than Value";
+					value = "UNITS"
+				};
+                class RP_VT_LESSTHANWEST {
+                    name = "Fewer BLUFOR units alive than Value";
+					value = "WEST"
+                };
+                class RP_VT_LESSTHANEAST {
+                    name = "Fewer OPFOR units alive than Value";
+					value = "EAST"
+					default = 1;
+                };
+                class RP_VT_LESSTHANINDEPENDENT {
+                    name = "Fewer INDEPENDENT units alive than Value";
+					value = "INDEPENDENT"
+                };
+                class RP_VT_LESSTHANCIVILIAN {
+                    name = "Fewer CIVILIAN units alive than Value";
+					value = "CIVILIAN"
+                };
+			};
+		};
+
+        class Value: Edit {
+			property = "RHNET_ab_moduleREPEATER_F_Value";
 			displayName = "Value";
 			tooltip = "Value to be monitored.";
 			typeName = "NUMBER";
+            defaultValue = "0";
 			control = "EditShort";
 		};
 
-		class ModuleDescription: ModuleDescription{};
-	};
+        class OptionsDescription: Edit {
+			property = "RHNET_ab_moduleREPEATER_F_OptionsDescription";
+            description = "With options below you can further adjust this repeater, no matter which activator you selected.";
+			displayName = "";
+			tooltip = "";
+			control = "SubCategoryNoHeader2";
+            data = "AttributeSystemSubcategory"
+		};
 
-	class ModuleDescription: ModuleDescription {
-		description = "Repeater controls Actionpoint repeating.";
-		sync[] = {"EmptyDetector","RHNET_ab_moduleAP_F"};
+        class RepeatInterval: Edit {
+			property = "RHNET_ab_moduleREPEATER_F_DelayBetweenRepeats";
+			displayName = "Repeat interval";
+			tooltip = "Delay between repeats in seconds (1: minimum allowed).";
+			typeName = "NUMBER";
+            defaultValue = "1";
+			control = "EditShort";
+		};
 
-		class RHNET_ab_moduleAP_F {
-			description = "There can be multiple Actionpoints synchronized.";
-			position = 0;
-			direction = 0;
-			duplicate = 1;
+        class MaximumRepeats: Edit {
+			property = "RHNET_ab_moduleREPEATER_F_MaximumRepeats";
+			displayName = "Maximum repeats";
+			tooltip = "How many times can this repeater trigger (-1: no limit).";
+			typeName = "NUMBER";
+            defaultValue = "0";
+			control = "EditShort";
+		};
+
+        class MonitorFPS: Checkbox {
+			property = "RHNET_ab_moduleREPEATER_F_MonitorFPS";
+			displayName = "Monitor FPS";
+			tooltip = "If enabled, the execution will halt until the server FPS is higher than 20.";
+			typeName = "BOOL";
+            defaultValue = "true";
 		};
 	};
 };
--- @actionbuilder/addons/actionbuilder/modules/repeater/fn_moduleRepeater.sqf
+++ @actionbuilder/addons/actionbuilder/modules/repeater/fn_moduleRepeater.sqf
@@ -20,72 +20,107 @@ if (_actionpoints isEqualTo []) exitWith {
 	false
 };
 
-// Repeater settings.
-private _activationType = _repeater getVariable ["ActivationType", "BOOLEAN"];
-private _customVariable = _repeater getVariable ["CustomVariable", ""];
-private _customValue = _repeater getVariable ["CustomValue", -1];
-private _requiresVariable = ["BOOLEAN"];
-private _requiresValue = ["MINUNITCOUNT", "MAXPLAYERCOUNT"];
+// Activation method selection.
+private _ActivationMethod = _repeater getVariable ["ActivationMethod", "VARIABLE"];
 
-if (
-	(_requiresVariable find _activationType != -1)
-	&& (_customVariable == "")
-) exitWith {
-	["Invalid variable given for Repeater %1.", _repeater] call BIS_fnc_error;
+// Boolean method.
+private _BooleanVariable = _repeater getVariable ["BooleanVariable", ""];
+private _ToggleVariable = _repeater getVariable ["ToggleVariable", true];
+
+// Value method.
+private _ValueCondition = _repeater getVariable ["ValueCondition", "LESSUNITS"];
+private _Value = _repeater getVariable ["Value", 0];
+
+// Options.
+private _RepeatInterval = _repeater getVariable ["RepeatInterval", 1];
+private _MaximumRepeats = _repeater getVariable ["MaximumRepeats", 0];
+private _MonitorFPS = _repeater getVariable ["MonitorFPS", true];
+
+// Make sure a variable is given if the selected activation method is boolean variable.
+if (_ActivationMethod == "VARIABLE" && _BooleanVariable == "") exitWith {
+	["Boolean variable cannot be empty! Check %1.", _repeater] call BIS_fnc_error;
 	false
 };
 
-if (
-	(_requiresValue find _activationType != -1)
-	&& (_customValue == -1)
-) exitWith {
-	["Invalid value given for Repeater %1.", _repeater] call BIS_fnc_error;
+// Make sure the variable exists if the selected activation method is boolean variable.
+if (_ActivationMethod == "VARIABLE" && isNil _BooleanVariable) then {
+	missionNamespace setVariable [_BooleanVariable, false];
+};
+
+// Too short repeating interval.
+if (_RepeatInterval < 1) exitWith {
+	["Repeat interval cannot be shorter than a one second! Check %1.", _repeater] call BIS_fnc_error;
 	false
 };
 
 // Start monitoring.
-while {true} do {
+private _c = 0;
+while {_MaximumRepeats == -1 || _c < _MaximumRepeats} do {
 	private _execute = false;
-	if (_activationType == "BOOLEAN") then {
-		// Make sure the variable is initialized.
-		if (isNil _customVariable) then {
-			missionNamespace setVariable [_customVariable, false];
-		};
-		// Read the variable value.
-		private _value = missionNamespace getVariable _customVariable;
-		// Value must be boolean.
+	if (_ActivationMethod == "VARIABLE") then {
+		// Activation method boolean variable.
+		private _value = missionNamespace getVariable _BooleanVariable;
 		if (typeName _value == "BOOL") then {
 			if (_value) then {
-				// Value of the variable is true.
-				// Set to execute and toggle the variable.
+				// Value of the boolean variable is TRUE.
+				// Set to execute and toggle the variable if requested.
 				_execute = true;
-				missionNamespace setVariable [_customVariable, false];
+				if (_ToggleVariable) then {
+					missionNamespace setVariable [_BooleanVariable, false];
+				};
 			};
 		};
 	};
-	if (_activationType == "MINUNITCOUNT") then {
-		if (count allUnits < _customValue) then {
-			// There are less units than the value.
-			_execute = true;
-		};
-	};
-	if (_activationType == "MAXPLAYERCOUNT") then {
-		if (count (playableUnits + switchableUnits) > _customValue) then {
-			// There are more alive players than the value.
-			_execute = true;
+	if (_ActivationMethod == "VALUE") then {
+		// Activation method value.
+		switch (_ValueCondition) do {
+			case "PLAYERS": {
+				if (count (playableUnits + switchableUnits) > _Value) then {
+					_execute = true;
+				};
+			};
+			case "UNITS": {
+				if (count allUnits < _Value) then {
+					_execute = true;
+				};
+			};
+			case "WEST": {
+				if ({(side _x) == west} count allUnits < _Value) then {
+					_execute = true;
+				};
+			};
+			case "EAST": {
+				if ({(side _x) == east} count allUnits < _Value) then {
+					_execute = true;
+				};
+			};
+			case "INDEPENDENT": {
+				if ({(side _x) == independent} count allUnits < _Value) then {
+					_execute = true;
+				};
+			};
+			case "CIVILIAN": {
+				if ({(side _x) == civilian} count allUnits < _Value) then {
+					_execute = true;
+				};
+			};
+			default {};
 		};
 	};
 	if (_execute) then {
-		// FPS must be over 20 for the repeater to work.
-		waitUntil {diag_fps > 20};
+		if (_MonitorFPS) then {
+			// FPS must be over 20 for the repeater to work.
+			waitUntil {diag_fps > 20};
+		};
 		{
 			[
 				_x,
 				[_x, false] call Actionbuilder_fnc_modulePortals
 			] execFSM "RHNET\rhnet_actionbuilder\modules\actionpoint\rhfsm_actionpoint.fsm";
 		} forEach _actionpoints;
+		_c = _c + 1;
 	};
-	sleep 1;
+	sleep _RepeatInterval;
 };
 
 true
